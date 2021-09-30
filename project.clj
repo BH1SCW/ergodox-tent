13b503fee5200d35dc1679e3e91c599865afcfa8
FanJun Kong <bh1scw@gmail.com>
2021-09-30 14:49:19 +0800

    Fix error on my laptop.

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "https://github.com/adereth/ergodox-tent"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [scad-clj "0.1.0"]])
+  :dependencies [[org.clojure/clojure "1.10.3"], [scad-clj "0.5.3"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
